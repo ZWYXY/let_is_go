490fec575011a6ba528804b2f96bc9d77de111aa
zhourui <1142165668@qq.com>
2021-06-15 19:30:06 +0800

    feat(new projects add files):add new files

---
+++ communication/2_version/Server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string // ip
+	Port int    // 端口
+	C    chan string
+	conn net.Conn
+}
+
+// NewServer 创建一个服务类
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+// BusinessHandler 业务处理类
+func (this *Server) BusinessHandler(conn net.Conn) {
+	fmt.Println("连接已建立")
+}
+
+// StartServer Server类的启动方法
+func (this *Server) StartServer() {
+	// 调用net包下的listen方法开启一个监听器
+	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	defer listen.Close()
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("net.Listen err:", err)
+			return
+		}
+		go this.BusinessHandler(conn)
+	}
+}
